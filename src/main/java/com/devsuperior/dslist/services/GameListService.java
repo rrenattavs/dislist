6e4fdff03480b63c9a090ee1c03aba6207508e92
Renata Vieira <rrenattavs@gmail.com>
2023-11-17 23:13:51 -0700

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+	
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public GameListDTO findById(Long id) {
+//		Game result = gameRepository.findById(id).get(); //get from the database, that can put directly in the next line
+		return new GameListDTO(gameListRepository.findById(id).get());
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll(){
+		List<GameList> result = gameListRepository.findAll();
+		return  result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+	
+}
