697efe45757aa49f2a8dfe78a70da527780123e7
Renata Vieira <rrenattavs@gmail.com>
2023-11-20 10:59:13 -0700

    insert a SQL query

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -9,13 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.projections.GameMinProjection;
+import com.devsuperior.dslist.repositories.GameRepository;
 import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
 
 @RestController
 @RequestMapping(value = "/lists")
 public class GameListController {
 	@Autowired
 	private GameListService gameListService;
+	
+	@Autowired
+	private GameService gameService;
 	
 	@GetMapping(value = "/{id}")
 	public GameListDTO findById(@PathVariable Long id){
@@ -25,5 +32,11 @@ public class GameListController {
 	@GetMapping
 	public List<GameListDTO> findAll(){
 		return gameListService.findAll();
+	}
+	
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable Long listId){
+		
+		return gameService.findByList(listId);
 	}
 }
